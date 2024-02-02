dda86cde1f0df048bd024fc9eebe6798eda20bbd
mila <mila.lozanova@proenviro.co.uk>
2024-02-02 15:04:09 +0000

    footer- 'subscribe' col improved and btn added

--- src/app/_partials/footer/footer.component.ts
+++ src/app/_partials/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {InputTextModule} from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-    imports: [
-        InputTextModule
-    ],
+  imports: [
+    InputTextModule,
+    ButtonModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
