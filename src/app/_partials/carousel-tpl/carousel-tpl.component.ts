97e8272797f4da44f5838ead02402bc3330c609f
mila <mila.lozanova@proenviro.co.uk>
2024-02-02 10:57:34 +0000

    new partial component created - carousel

---
+++ src/app/_partials/carousel-tpl/carousel-tpl.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel-tpl',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel-tpl.component.html',
+  styleUrl: './carousel-tpl.component.scss'
+})
+export class CarouselTplComponent {
+
+}
