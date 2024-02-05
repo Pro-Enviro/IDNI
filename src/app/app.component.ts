6de3ea041d6e169d31d83863e47557c62f7d07f1
adamshelley <adam.shelley@proenviro.co.uk>
2024-02-05 14:28:43 +0000

    - Adding landing page component - Adding PET page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import {LandingTemplateComponent} from "./_templates/landing-template/landing-te
 import {HeaderTopComponent} from "./_partials/header-top/header-top.component";
 import {HeroComponent} from "./_partials/hero/hero.component";
 import {InfoComponent} from "./_partials/info/info.component";
+import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
+import {PetComponent} from "./pages/pet/pet.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderTopComponent, HeroComponent, InfoComponent],
+  imports: [CommonModule, RouterOutlet, HeaderTopComponent, HeroComponent, InfoComponent, LandingPageComponent, PetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {PetComponent} from "./pages/pet/pet.component";
+import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'pet', component: PetComponent},
+  {path: '**', redirectTo: ''}
+];
