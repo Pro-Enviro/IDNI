1760ba388c1169855dcb1437ef40f78a2375c397
SillyMonkey1925 <rian.jacobs@proenviro.co.uk>
2024-02-06 08:55:58 +0000

    added under construction video and image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import {PetComponent} from "./pages/pet/pet.component";
 import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
+import {ConstructionComponent} from "./_partials/construction/construction.component";
 
 export const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'pet', component: PetComponent},
+  {path: 'coming-soon', component: ConstructionComponent},
+
   {path: '**', redirectTo: ''}
 ];
