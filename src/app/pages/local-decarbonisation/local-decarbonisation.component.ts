b5b0058e6aef0d9b37565d59ad8cf35807307b24
mila <mila.lozanova@proenviro.co.uk>
2024-02-08 12:13:30 +0000

    local authority - multipage created with img and text

--- src/app/pages/local-decarbonisation/local-decarbonisation.component.ts
+++ src/app/pages/local-decarbonisation/local-decarbonisation.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {TopPageImgTplComponent} from "../../_partials/top-page-img-tpl/top-page-img-tpl.component";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-local-decarbonisation',
   standalone: true,
   imports: [
-    TopPageImgTplComponent
+    TopPageImgTplComponent,
+    ButtonModule
   ],
   templateUrl: './local-decarbonisation.component.html',
   styleUrl: './local-decarbonisation.component.scss'
