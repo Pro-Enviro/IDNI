dcce7e703146f0592c09cea3f3ab44eb65892133
mila <mila.lozanova@proenviro.co.uk>
2024-02-08 11:29:42 +0000

    new page created - local decarbonisation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import {PetComponent} from "./pages/pet/pet.component";
 import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
 import {ConstructionComponent} from "./_partials/construction/construction.component";
+import {LocalDecarbonisationComponent} from "./pages/local-decarbonisation/local-decarbonisation.component";
 
 export const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'pet', component: PetComponent},
   {path: 'coming-soon', component: ConstructionComponent},
+  {path:'local-decarbonisation', component:LocalDecarbonisationComponent},
 
   {path: '**', redirectTo: ''}
 ];
---
+++ src/app/pages/local-decarbonisation/local-decarbonisation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-local-decarbonisation',
+  standalone: true,
+  imports: [],
+  templateUrl: './local-decarbonisation.component.html',
+  styleUrl: './local-decarbonisation.component.scss'
+})
+export class LocalDecarbonisationComponent {
+
+}
