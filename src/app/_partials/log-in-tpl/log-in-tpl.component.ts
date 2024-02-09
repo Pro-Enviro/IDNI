a075aecbdaec36492f62ab2bff7512bc26f6d7ed
mila <mila.lozanova@proenviro.co.uk>
2024-02-09 12:08:10 +0000

    log in card created

--- src/app/_partials/log-in-tpl/log-in-tpl.component.ts
+++ src/app/_partials/log-in-tpl/log-in-tpl.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
 
 @Component({
   selector: 'app-log-in-tpl',
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextModule,
+    ButtonModule,
+    RippleModule
+  ],
   templateUrl: './log-in-tpl.component.html',
   styleUrl: './log-in-tpl.component.scss'
 })
