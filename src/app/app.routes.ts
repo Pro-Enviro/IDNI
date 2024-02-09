09be200bb4259658d55f17ec81099fa03c4aa616
mila <mila.lozanova@proenviro.co.uk>
2024-02-09 11:59:56 +0000

    router link created for log in page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {LandingPageComponent} from "./pages/landing-page/landing-page.component"
 import {ConstructionComponent} from "./_partials/construction/construction.component";
 import {LocalDecarbonisationComponent} from "./pages/local-decarbonisation/local-decarbonisation.component";
 import {LocalDecabSingleTplComponent} from "./_partials/local-decab-single-tpl/local-decab-single-tpl.component";
+import {LogInTplComponent} from "./_partials/log-in-tpl/log-in-tpl.component";
 
 export const routes: Routes = [
   {path: '', component: LandingPageComponent},
@@ -12,6 +13,6 @@ export const routes: Routes = [
   {path:'local-decarbonisation', component:LocalDecarbonisationComponent,children:[
       {path:'local-decarb-single',component:LocalDecabSingleTplComponent}
     ]},
-
+  {path:'login',component:LogInTplComponent},
   {path: '**', redirectTo: ''}
 ];
