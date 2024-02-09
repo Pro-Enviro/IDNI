b28d42234d26d9203e9fcb93e4891a18ac689e87
mila <mila.lozanova@proenviro.co.uk>
2024-02-09 14:18:54 +0000

    registration form - questions ordered

--- src/app/_partials/registration-form-tpl/registration-form-tpl.component.ts
+++ src/app/_partials/registration-form-tpl/registration-form-tpl.component.ts
@@ -5,6 +5,11 @@ import {ButtonModule} from "primeng/button";
 import {RadioButtonModule} from "primeng/radiobutton";
 import {InputNumberModule} from "primeng/inputnumber";
 import {InputTextModule} from "primeng/inputtext";
+import {InputGroupAddonModule} from "primeng/inputgroupaddon";
+import {InputGroupModule} from "primeng/inputgroup";
+import {FormsModule} from "@angular/forms";
+import {InputTextareaModule} from "primeng/inputtextarea";
+
 
 @Component({
   selector: 'app-registration-form-tpl',
@@ -15,11 +20,16 @@ import {InputTextModule} from "primeng/inputtext";
     ButtonModule,
     RadioButtonModule,
     InputNumberModule,
-    InputTextModule
+    InputTextModule,
+    InputGroupAddonModule,
+    InputGroupModule,
+    FormsModule,
+    FormsModule,
+    InputTextareaModule
   ],
   templateUrl: './registration-form-tpl.component.html',
   styleUrl: './registration-form-tpl.component.scss'
 })
 export class RegistrationFormTplComponent {
-
+text?:string;
 }
