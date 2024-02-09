82bfe593678eb31e0b3a44fa472010d52f2a2fbe
mila <mila.lozanova@proenviro.co.uk>
2024-02-09 12:19:40 +0000

    registration form component created under partials

---
+++ src/app/_partials/registration-form-tpl/registration-form-tpl.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-form-tpl',
+  standalone: true,
+  imports: [],
+  templateUrl: './registration-form-tpl.component.html',
+  styleUrl: './registration-form-tpl.component.scss'
+})
+export class RegistrationFormTplComponent {
+
+}
