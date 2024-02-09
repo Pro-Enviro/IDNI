165772b1c89f2dfc514c5e34cc8cb26ec01d6067
mila <mila.lozanova@proenviro.co.uk>
2024-02-09 11:52:07 +0000

    log in component created

---
+++ src/app/_partials/log-in-tpl/log-in-tpl.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-tpl',
+  standalone: true,
+  imports: [],
+  templateUrl: './log-in-tpl.component.html',
+  styleUrl: './log-in-tpl.component.scss'
+})
+export class LogInTplComponent {
+
+}
