cbe9dceeb49712dc6f500ed3abc86dbd365318cf
mila <mila.lozanova@proenviro.co.uk>
2024-02-13 15:10:39 +0000

    routerlink created for local decarbonisation single page

--- src/app/_partials/local-decab-single-tpl/local-decab-single-tpl.component.ts
+++ src/app/_partials/local-decab-single-tpl/local-decab-single-tpl.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TopPageImgTplComponent} from "../top-page-img-tpl/top-page-img-tpl.component";
 
 @Component({
   selector: 'app-local-decab-single-tpl',
   standalone: true,
-  imports: [],
+  imports: [
+    TopPageImgTplComponent
+  ],
   templateUrl: './local-decab-single-tpl.component.html',
   styleUrl: './local-decab-single-tpl.component.scss'
 })
