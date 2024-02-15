f5ec604209359a9d83e7ddfcd460ebe0e59bf8a5
mila <mila.lozanova@proenviro.co.uk>
2024-02-15 11:16:32 +0000

    live chat title changed, ngFor/ngIf/ngElse used in the html code

--- src/app/_partials/chat-dialog-tpl/live-chat-tpl/live-chat-tpl.component.ts
+++ src/app/_partials/chat-dialog-tpl/live-chat-tpl/live-chat-tpl.component.ts
@@ -1,6 +1,6 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import {ButtonModule} from "primeng/button";
-import {DatePipe} from "@angular/common";
+import {DatePipe, NgForOf, NgIf} from "@angular/common";
 
 export interface chat{
   name: string;
@@ -9,26 +9,40 @@ export interface chat{
   dateTime: Date;
   message: string;
   status: string; //send or recieve
+
 }
 
 @Component({
   selector: 'app-live-chat-tpl',
   standalone: true,
-    imports: [
-        ButtonModule
-    ],
+  imports: [
+    ButtonModule,
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './live-chat-tpl.component.html',
   styleUrl: './live-chat-tpl.component.scss'
 })
 export class LiveChatTplComponent {
+  //NEW CODE
+  messages: chat[] = [];
+  send: boolean = true;
+  dateTime?: Date = new Date();
+  private message: any;
+
+  sendMessage= () => {
+    console.log('magic');
+    this.message.dateTime = <string>(new DatePipe('en-US').transform(new Date(), 'hh:mm:ss'));
+  }
+
+
+
+  //OLD CODE
   @ViewChild('inputBox') inputBoxRef?:ElementRef;
   @ViewChild('timeBox') timeBoxRef?:ElementRef;
-  chat?:any;
-  inputBox?:any
-  date?: Date = new Date();
-
-  messages: chat[] = [];
-
+  // chat?:any;
+  // inputBox?:any
+  // date?: Date = new Date();
 
   assistantPhrases ?:any = [
   "Welcome !",
@@ -40,89 +54,89 @@ export class LiveChatTplComponent {
   "For more information send us a letter by the post"
   ]
 
-  sendMessage= () => {
-    const day = new Date();
-    console.log(this.date);
-  let randomPhrase = Math.floor(Math.random()* this.assistantPhrases.length);
-  let assistantMessageText = this.assistantPhrases[randomPhrase];
+  // sendMessage= () => {
+  //   const day = new Date();
+  //   console.log(this.date);
+  // let randomPhrase = Math.floor(Math.random()* this.assistantPhrases.length);
+  // let assistantMessageText = this.assistantPhrases[randomPhrase];
+  //
+  // // Creating a DIV
+  // const userDiv = document.createElement("div");
+  //  userDiv.classList.add("user-container");
+  //
+  //  //USER P TAG
+  //  const userP= document.createElement("p");
+  //
+  //  //TIME P TAG
+  //  const timeBox = document.createElement('span');
+  //  timeBox.classList.add("time-style");
+  //  //TIME FORMAT
+  //   timeBox.innerHTML = <string>(new DatePipe('en-US').transform(new Date(), 'dd-MM-yyyy hh:mm:ss'));
+  //   timeBox.innerHTML = <string>(new DatePipe('en-US').transform(new Date(), 'hh:mm:ss'));
+  //   timeBox.innerHTML = this.date?.toLocaleString("en-US") || '';
+  //
+  //
+  //  this.chat = document.querySelector(".chat");
+  //  this.inputBox = document.querySelector(".inputText");
+  //  userP.innerHTML = this.inputBox.value;
+  //  userP.classList.add("userMsg");
+  //  userDiv.append(userP);
+  //  userDiv.append(timeBox);
+  //  this.chat.appendChild(userDiv);
+  //  this.inputBox.value = "";
+  //  this.chat.scrollTop = this.chat.scrollHeight;
+  //
+  //  let typing = document.createElement("p");
+  //  typing.innerHTML = "Assistant is typing ...";
+  //  typing.classList.add("typing-text");
+  //  this.chat.appendChild(typing);
+  //  setTimeout(()=>{
+  //    let assistDiv = document.createElement("div");
+  //    let assistP = document.createElement("p");
+  //    //TIME P TAG
+  //    const timeBox = document.createElement('span');
+  //    timeBox.classList.add("time-style");
+  //    timeBox.innerHTML = <string>(new DatePipe('en-US').transform(new Date(), 'hh:mm:ss'));
+  //    assistP.innerHTML = assistantMessageText;
+  //    assistP.classList.add("assistantMsg");
+  //    assistDiv.classList.add("assistant-container");
+  //    assistDiv.append(assistP);
+  //    assistDiv.append(timeBox);
+  //    this.chat.appendChild(assistDiv);
+  //    this.chat.scrollTop = this.chat.scrollHeight;
+  //    this.chat.removeChild(typing);
+  //  },2000)
+  // }
 
-  // Creating a DIV
-  const userDiv = document.createElement("div");
-   userDiv.classList.add("user-container");
 
-   //USER P TAG
-   const userP= document.createElement("p");
-
-   //TIME P TAG
-   const timeBox = document.createElement('span');
-   timeBox.classList.add("time-style");
-   //TIME FORMAT
-    //timeBox.innerHTML = <string>(new DatePipe('en-US').transform(new Date(), 'dd-MM-yyyy hh:mm:ss'));
-    timeBox.innerHTML = <string>(new DatePipe('en-US').transform(new Date(), 'hh:mm:ss'));
-    //timeBox.innerHTML = this.date?.toLocaleString("en-US") || '';
-
-
-   this.chat = document.querySelector(".chat");
-   this.inputBox = document.querySelector(".inputText");
-   userP.innerHTML = this.inputBox.value;
-   userP.classList.add("userMsg");
-   userDiv.append(userP);
-   userDiv.append(timeBox);
-   this.chat.appendChild(userDiv);
-   this.inputBox.value = "";
-   this.chat.scrollTop = this.chat.scrollHeight;
-
-   let typing = document.createElement("p");
-   typing.innerHTML = "Assistant is typing ...";
-   typing.classList.add("typing-text");
-   this.chat.appendChild(typing);
-   setTimeout(()=>{
-     let assistDiv = document.createElement("div");
-     let assistP = document.createElement("p");
-     //TIME P TAG
-     const timeBox = document.createElement('span');
-     timeBox.classList.add("time-style");
-     timeBox.innerHTML = <string>(new DatePipe('en-US').transform(new Date(), 'hh:mm:ss'));
-     assistP.innerHTML = assistantMessageText;
-     assistP.classList.add("assistantMsg");
-     assistDiv.classList.add("assistant-container");
-     assistDiv.append(assistP);
-     assistDiv.append(timeBox);
-     this.chat.appendChild(assistDiv);
-     this.chat.scrollTop = this.chat.scrollHeight;
-     this.chat.removeChild(typing);
-   },2000)
-  }
-
-
-  onKey = (event: any) => {
-    if (event === "Enter") {
-    let random = Math.floor(Math.random()*this.assistantPhrases.length);
-    let assistantTextBox = this.assistantPhrases[random];
-    const userDiv2 = document.createElement("div");
-    userDiv2.classList.add("user-container");
-    let userP2 = document.createElement("p");
-    userP2.innerHTML = this.inputBox.value;
-    userP2.classList.add("userMsg");
-    userDiv2.append(userP2);
-    this.chat.appendChild(userDiv2);
-    this.inputBox.value = "";
-    this.chat.scrollTop = this.chat.scrollHeight;
-
-    let type = document.createElement("p");
-    type.innerHTML = "Assistant is typing...";
-    type.classList.add("typing-text");
-    this.chat.appendChild(type);
-    setTimeout(() => {
-      let assistantMessage = document.createElement("p");
-      assistantMessage.innerHTML = assistantTextBox;
-      assistantMessage.classList.add("assistantMsg");
-      this.chat.appendChild(assistantMessage);
-      this.chat.scrollTop = this.chat.scrollHeight;
-      this.chat.remove(type);
-
-    },2000)
-  }
-  }
+  // onKey = (event: any) => {
+  //   if (event === "Enter") {
+  //   let random = Math.floor(Math.random()*this.assistantPhrases.length);
+  //   let assistantTextBox = this.assistantPhrases[random];
+  //   const userDiv2 = document.createElement("div");
+  //   userDiv2.classList.add("user-container");
+  //   let userP2 = document.createElement("p");
+  //   userP2.innerHTML = this.inputBox.value;
+  //   userP2.classList.add("userMsg");
+  //   userDiv2.append(userP2);
+  //   this.chat.appendChild(userDiv2);
+  //   this.inputBox.value = "";
+  //   this.chat.scrollTop = this.chat.scrollHeight;
+  //
+  //   let type = document.createElement("p");
+  //   type.innerHTML = "Assistant is typing...";
+  //   type.classList.add("typing-text");
+  //   this.chat.appendChild(type);
+  //   setTimeout(() => {
+  //     let assistantMessage = document.createElement("p");
+  //     assistantMessage.innerHTML = assistantTextBox;
+  //     assistantMessage.classList.add("assistantMsg");
+  //     this.chat.appendChild(assistantMessage);
+  //     this.chat.scrollTop = this.chat.scrollHeight;
+  //     this.chat.remove(type);
+  //
+  //   },2000)
+  // }
+  // }
 
 }
