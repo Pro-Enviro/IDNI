9c042b7587ed960ec26738b5a5a823aa00110d22
mila <mila.lozanova@proenviro.co.uk>
2024-02-16 14:26:48 +0000

    email validation added to chat-dialog

--- src/app/_partials/chat-dialog-tpl/chat-dialog-tpl.component.ts
+++ src/app/_partials/chat-dialog-tpl/chat-dialog-tpl.component.ts
@@ -4,9 +4,10 @@ import {InputTextModule} from "primeng/inputtext";
 import {ButtonModule} from "primeng/button";
 import {RippleModule} from "primeng/ripple";
 import {LiveChatTplComponent} from "./live-chat-tpl/live-chat-tpl.component";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormsModule, Validators} from "@angular/forms";
 import {AuthService} from "../../_services/users/auth.service";
 import {Router, RouterLink} from "@angular/router";
+import {NgClass, NgIf} from "@angular/common";
 
 
 export interface formChat{
@@ -14,6 +15,8 @@ export interface formChat{
   company_name?: string;
   email?: string;
 }
+
+
 @Component({
   selector: 'app-chat-dialog-tpl',
   standalone: true,
@@ -24,34 +27,36 @@ export interface formChat{
     RippleModule,
     LiveChatTplComponent,
     FormsModule,
-    RouterLink
+    RouterLink,
+    NgClass,
+    NgIf
   ],
   templateUrl: './chat-dialog-tpl.component.html',
   styleUrl: './chat-dialog-tpl.component.scss'
 })
+
+
 export class ChatDialogTplComponent {
-
-
 
   name?: string;
   company_name?: string;
-  email?: string;
+  email?:string;
 
   form: formChat[] = [];
 
+
   startChat() {
-    if (!this.name || !this.company_name || !this.email) {
-      this.form.push({
-        name: '',
-        email: '',
-        company_name: '',
-      })
-      return;
+
+    let userLogIn = {
+      name: this.name,
+      email: this.email,
+      company_name: this.company_name
     }
+
+    this.form.push(userLogIn);
     this.name = '';
     this.company_name = '';
     this.email = '';
-
 
   }
 }
