c1e003f11a1ddb453f089f522f896d5ec9bf826d
mila <mila.lozanova@proenviro.co.uk>
2024-02-27 09:23:59 +0000

    new component created for msw page + information added

---
+++ src/app/_partials/msx-tpl/msx-tpl.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {TopPageImgTplComponent} from "../top-page-img-tpl/top-page-img-tpl.component";
+
+@Component({
+  selector: 'app-msx-tpl',
+  standalone: true,
+  imports: [
+    TopPageImgTplComponent
+  ],
+  templateUrl: './msx-tpl.component.html',
+  styleUrl: './msx-tpl.component.scss'
+})
+export class MsxTplComponent {
+
+}
