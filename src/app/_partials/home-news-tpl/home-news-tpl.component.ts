1dc0f2d03f0aa69cb2837b3b6bcca6f04b6dcc76
SillyMonkey1925 <rian.jacobs@proenviro.co.uk>
2024-02-28 10:16:50 +0000

    First part of Database Integration Complete

--- src/app/_partials/home-news-tpl/home-news-tpl.component.ts
+++ src/app/_partials/home-news-tpl/home-news-tpl.component.ts
@@ -14,9 +14,9 @@ import {NewsCardsTplComponent} from "../news-cards-tpl/news-cards-tpl.component"
 export class HomeNewsTplComponent {
   newsArticles: any;
   constructor(private db: DbService) {
-    this.db.getContentFromCollection(`news_pages`, '?fields=title,news_articles.content_with_image_type_id.*').subscribe({
+    this.db.getContentFromCollection(`news_pages/1`, '?fields=title,news_articles.content_with_image_type_id.*').subscribe({
       next: (res: any) => {
-        this.newsArticles = res[0]
+        this.newsArticles = res
         this.newsArticles.news_articles = this.newsArticles.news_articles.map((article:any) =>  article.content_with_image_type_id)
       },
       error: (err:any) => console.error(err)
