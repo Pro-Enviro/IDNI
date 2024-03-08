1940abc3753b90d6157d3d5bed30950f8646c600
mila <mila.lozanova@proenviro.co.uk>
2024-03-08 15:19:54 +0000

    api call made for partners page

--- src/app/pages/partners/partners-content-tpl/partners-content-tpl.component.ts
+++ src/app/pages/partners/partners-content-tpl/partners-content-tpl.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-partners-content-tpl',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule
+  ],
   templateUrl: './partners-content-tpl.component.html',
   styleUrl: './partners-content-tpl.component.scss'
 })
 export class PartnersContentTplComponent {
-
+  @Input('content') content: any;
 }
--- src/app/pages/partners/partners.component.ts
+++ src/app/pages/partners/partners.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
 import {PartnersContentTplComponent} from "./partners-content-tpl/partners-content-tpl.component";
+import {TopPageImgTplComponent} from "../../_partials/top-page-img-tpl/top-page-img-tpl.component";
+import {DbService} from "../../_services/db.service";
 
 @Component({
   selector: 'app-partners',
   standalone: true,
   imports: [
-    PartnersContentTplComponent
+    PartnersContentTplComponent,
+    TopPageImgTplComponent
   ],
   templateUrl: './partners.component.html',
   styleUrl: './partners.component.scss'
 })
 export class PartnersComponent {
+partners:any;
 
+  constructor(private db: DbService) {
+    this.db.getContentFromCollection('partners/1', `
+?fields=title,
+content,
+top_image,
+items.partner_items_id.title,
+items.partner_items_id.content,
+items.partner_items_id.image
+`).subscribe({
+      next: (res: any) => {
+        this.partners = res
+        this.partners.items = this.partners.items.map((item:any) => item.partner_items_id)
+      },
+      error: (err: any) => {
+        console.error(err)
+      }
+    })
+  }
 }
