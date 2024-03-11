fdfea3c87974babc30c081f8a5217a2ebfe2125c
mila <mila.lozanova@proenviro.co.uk>
2024-03-11 11:38:34 +0000

    partners - responsive col added & partner-content component deleted

--- src/app/pages/partners/partners.component.ts
+++ src/app/pages/partners/partners.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import {PartnersContentTplComponent} from "./partners-content-tpl/partners-content-tpl.component";
 import {TopPageImgTplComponent} from "../../_partials/top-page-img-tpl/top-page-img-tpl.component";
 import {DbService} from "../../_services/db.service";
 import {SinglePartnerComponent} from "./single-partner/single-partner.component";
 import {JsonPipe} from "@angular/common";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-partners',
   standalone: true,
   imports: [
-    PartnersContentTplComponent,
     TopPageImgTplComponent,
     SinglePartnerComponent,
-    JsonPipe
+    JsonPipe,
+    ButtonModule
   ],
   templateUrl: './partners.component.html',
   styleUrl: './partners.component.scss'
@@ -38,11 +38,15 @@ partner_single.related_partners_id.top_image
       next: (res: any) => {
         this.partners = res
         this.partners.items = this.partners.items.map((item:any) => item.partner_items_id)
-        this.partners.partner_single = this.partners.partner_single.map((article:any) => article.related_partners_id)
+        //this.partners.partner_single = this.partners.partner_single.map((article:any) => article.related_partners_id)
       },
       error: (err: any) => {
         console.error(err)
       }
     })
+  }
+
+  getPage = (id:number) =>{
+
   }
 }
