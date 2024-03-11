61c145bf6a04708f095e267a31dfc3591f5366a3
adamshelley31 <148070121+adamshelley31@users.noreply.github.com>
2024-03-11 15:16:59 +0000

    - Built new classes to deal with updated excel spreadsheet. - Added collapsable table for the pet tool

--- src/app/pages/pet/pet.component.ts
+++ src/app/pages/pet/pet.component.ts
@@ -1,8 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-// import {AdminService} from "../../../_services/admin.service";
-// import {GlobalService} from "../../../_services/global.service";
 import {FormsModule} from "@angular/forms";
-// import {MessageService} from "primeng/api";
 import {PanelModule} from 'primeng/panel';
 import {SelectButtonModule} from "primeng/selectbutton";
 import {TableModule} from "primeng/table";
@@ -10,23 +7,74 @@ import {InputNumberModule} from "primeng/inputnumber";
 import {ButtonModule} from "primeng/button";
 import {CarouselTplComponent} from "../../_partials/carousel-tpl/carousel-tpl.component";
 import {FooterComponent} from "../../_partials/footer/footer.component";
+import {RippleModule} from "primeng/ripple";
+import {CommonModule, JsonPipe} from "@angular/common";
+import {DropdownModule} from "primeng/dropdown";
 
 const rowNames: string[] = ['Cost of Energy', 'Transportation Costs', 'Cost of Water', 'Cost of Waste', 'Cost of Raw Materials', 'Cost of Bought in Goods - Consumables and bought in parts', 'Consultancy Cost', 'Sub Contracting Cost', 'Other External Costs (Legal, rental, accounting etc)']
-const energyNames: string[] = ['Oil', 'Gas', 'LPG', 'Electricity', 'Diesel', 'Kerosene', 'Other']
-const gridAllocationNames: string[] = ['kVa Availability', 'Recorded Winter Max Demand kVa']
-const onSiteNames: string[] = ['PV', 'Wind', 'Solar Thermal', 'CHP', 'Biomass', 'Hydro', 'AD', 'Other']
+
+const energyNames: string[] = ['Electricity','Natural Gas (Grid)', 'Natural Gas off Grid', 'Bio Gas Off Grid', 'LPG','Oil','Kerosene','Bio Fuels','Bio Mass','Coal for Industrial use','Other']
+const materialNames: string[] = ['Steel','Stainless Steel','Aluminium','Copper','Bronze','Titanium','Polymers','Elastomers','Textiles','Composites','Aggregates','Cement','Glass','Wood','Chemicals','Lithium','Magnesium','Other']
+
+// const gridAllocationNames: string[] = ['kVa Availability', 'Recorded Winter Max Demand kVa']
+// const onSiteNames: string[] = ['PV', 'Wind', 'Solar Thermal', 'CHP', 'Biomass', 'Hydro', 'AD', 'Other']
+
+type UnitsUom = 'Select' | 'litres' | 'kg' |'kWh' | 'tonnes' | 'cubic metres' | 'km' | 'miles'
+type RegionsOfOrigin = 'UK' | 'EU' | 'US' | 'Asia'
 
 
+
+class BoughtInParts {
+  name: string = 'Description of Part'
+  secondColumn?:number = 0
+  NoOfParts: number = 0
+  cost: number = 0
+  unitOfCost: 'Cost/unit' | 'Total Cost' | 'Select' = 'Select'
+  regionOfOrigin: RegionsOfOrigin  = 'UK'
+  parent = {
+    name: '',
+    addRows: true
+  }
+}
 class TableRow {
   name: string = ''
-  value: number = 0
+  secondColumn: number = 0 ;
+  unitsUom: UnitsUom = 'Select'
+  totalUnits: number = 0
+  cost: number = 0
+  unitOfCost?: 'Select' | 'Total Cost' | 'Cost/unit' = 'Select'
+  regionOfOrigin?: RegionsOfOrigin = 'UK'
+  parent?: {name: string} = {
+    name: ''
+  }
+}
+
+class MaterialRow {
+  name: string = ''
+  secondColumn: number = 0 ;
+  unitsUom: UnitsUom = 'Select'
+  totalUnits: number = 0
+  cost: number = 0
+  regionOfOrigin?: RegionsOfOrigin = 'UK'
+  scrappageAndWaste?: number = 0
+  parent?: {name: string} = {
+    name: ''
+  }
+}
+
+class GroupItem {
+  name: string = ''
+  value: number = 0;
   secondColumn = 0
+  parent: {name: string} = {
+    name: ''
+  }
 }
 
 @Component({
   selector: 'app-pet',
   standalone: true,
-  imports: [FormsModule, PanelModule, SelectButtonModule, TableModule, InputNumberModule, ButtonModule, CarouselTplComponent, FooterComponent],
+  imports: [CommonModule, FormsModule, PanelModule, SelectButtonModule, TableModule, InputNumberModule, ButtonModule, CarouselTplComponent, FooterComponent, RippleModule, JsonPipe, DropdownModule],
   templateUrl: './pet.component.html',
   styleUrl: './pet.component.scss'
 })
@@ -46,13 +94,13 @@ export class PetComponent implements OnInit {
   energyRows: TableRow[] = []
   gridAllocationRows: TableRow[] = []
   onSiteRows: TableRow[] = []
+  unitsUom: UnitsUom[] = ['Select' , 'litres' , 'kg' ,'kWh' , 'tonnes' , 'cubic metres' , 'km' , 'miles']
+  regionOfOrigin: RegionsOfOrigin[] = ['UK' , 'EU' , 'US' , 'Asia']
 
+  data: any = []
+  initialParts = new BoughtInParts()
 
-  constructor(
-    // private admin: AdminService,
-    // private global: GlobalService,
-    // private msg: MessageService
-  ) {
+  constructor() {
   }
 
   onSelectCompany = () => {
@@ -76,7 +124,7 @@ export class PetComponent implements OnInit {
     //   next: (res: any) => {
     //     this.companies = res.data.companies.map((x: any) => x.companies_id);
     //     this.selectedCompany = this.companies[0].id;
-        this.getPETReport(this.selectedCompany)
+    this.getPETReport(this.selectedCompany)
     //   }
     // })
   }
@@ -103,28 +151,56 @@ export class PetComponent implements OnInit {
     //     }
     //   }})
 
-    this.generateRows(rowNames, this.rows)
-    this.generateRows(energyNames, this.energyRows)
-    this.generateRows(gridAllocationNames, this.gridAllocationRows)
-    this.generateRows(onSiteNames, this.onSiteRows)
+
+    this.generateClasses('Cost of Energy', TableRow, energyNames)
+    this.generateClasses('Cost of Raw Materials ', MaterialRow, materialNames)
+    this.generateClasses('Cost of Bought in Goods - Consumables and bought in parts', BoughtInParts)
   }
 
-  generateRows = (array: string[], newArray: TableRow[]) => {
-    array.forEach((name: string) => {
-      let newRow = new TableRow();
-      newRow.name = name
-      newArray.push(newRow)
-    })
+  generateClasses = (rowTitle: string, classToUse: any, namesArray?: string[]) => {
+    if (namesArray?.length) {
+      const classArray = namesArray.map((name: string) => {
+          let newClass = new classToUse()
+          newClass.name = name
+          newClass.parent.name = rowTitle
+          if (newClass.parent.addRows) {
+            newClass.parent.addRows = true
+          }
+          return newClass;
+      })
+      this.data.push(...classArray)
+      console.log(this.data)
+    } else {
+      console.log('No names to use')
+      let newClass = new classToUse()
+      this.generateRows(newClass, rowTitle, true)
+    }
   }
+
+  generateRows = (array: string[] | any, parentName: string, isClass?: boolean) => {
+    if (isClass) {
+      array.parent.name = parentName
+      this.data.push(array)
+    } else {
+      array.forEach((name: string) => {
+        let newGroupItem = new GroupItem()
+        newGroupItem.name = name;
+        newGroupItem.parent.name = parentName
+        this.data.push(newGroupItem)
+      })
+    }
+  }
+
+
 
   calculatePerEmployeeCost = () => {
     let subTotal = 0;
     this.rows = this.rows.map((row: TableRow) => {
       // Calculate per employee
       if (!this.employees) return row;
-      row.secondColumn = row.value / this.employees;
+      row.secondColumn = row.cost / this.employees;
       // Calculate New total
-      subTotal += row.value;
+      subTotal += row.cost;
       this.totalOfRows = subTotal;
       // Recalculate Productivity Score
       this.productivityScore = (this.turnover - this.totalOfRows) / this.employees
