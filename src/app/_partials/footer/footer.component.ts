aabf4abc43f9233bc125d3c938d9f2570f2782b5
mila <mila.lozanova@proenviro.co.uk>
2024-03-13 13:59:43 +0000

    footer links fixed

--- src/app/_partials/footer/footer.component.ts
+++ src/app/_partials/footer/footer.component.ts
@@ -1,17 +1,74 @@
 import { Component } from '@angular/core';
 import {InputTextModule} from "primeng/inputtext";
 import {ButtonModule} from "primeng/button";
+import {MenuItem} from "primeng/api";
+import {PanelMenuModule} from "primeng/panelmenu";
+import {MenuModule} from "primeng/menu";
+import {MenubarModule} from "primeng/menubar";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    PanelMenuModule,
+    MenuModule,
+    MenubarModule
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  items: MenuItem[] = [
+    {
+      label: 'Home',
+      routerLink: '/home'
+    },
+    {
+      label: 'Project Information',
+      items:[
+        {
+          label:'Project Information',
+          routerLink:'/project-information'
+        },
+        {
+          label: 'Partners',
+          routerLink:'/partners'
+        },
+        {
+          label: 'Stakeholders',
+          routerLink:'/stakeholders'
+        },
+        {
+          label:'Advisory Board',
+          routerLink:'/advisory-board'
+        },
+        {
+          label:'Latest Technologies',
+          routerLink:'latest-technologies'
+        }
+      ]
+    },
+    {
+      label: 'NI Councils',
+      routerLink: 'local-decarbonisation'
+    },
+    {
+      label: 'Events',
+      routerLink: 'events'
+    },
+    {
+      label: 'News',
+      routerLink: 'coming-soon',
+    },
+    {
+      label: 'COSI\'s',
+      routerLink: 'coming-soon',
+    },
+    {
+      label: 'Funding',
+      routerLink: 'coming-soon',
+    }
+  ];
 }
