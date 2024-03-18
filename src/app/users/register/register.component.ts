150a693a93cda85856b7c7e2e259d2fdf9464b1a
mila <mila.lozanova@proenviro.co.uk>
2024-03-18 11:13:47 +0000

    video updated on events past events & first and last name fields added to registration form

--- src/app/users/register/register.component.ts
+++ src/app/users/register/register.component.ts
@@ -39,7 +39,8 @@ import {DropdownModule} from "primeng/dropdown";
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-  name?:string;
+  first_name?:string;
+  last_name?:string;
   email?:string;
   phone_number?:string;
   password?:string;
