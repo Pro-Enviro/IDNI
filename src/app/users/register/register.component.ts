15defc0229c85b49e58ead08c46d6cab31423497
mila <mila.lozanova@proenviro.co.uk>
2024-03-18 16:01:50 +0000

    checkInputs() created for form validation

--- src/app/users/register/register.component.ts
+++ src/app/users/register/register.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ButtonModule} from "primeng/button";
 import {CalendarModule} from "primeng/calendar";
 import {CheckboxModule} from "primeng/checkbox";
 import {ChipsModule} from "primeng/chips";
-import {FormsModule, NgForm} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, NgForm, Validators} from "@angular/forms";
 import {InputGroupAddonModule} from "primeng/inputgroupaddon";
 import {InputGroupModule} from "primeng/inputgroup";
 import {InputNumberModule} from "primeng/inputnumber";
@@ -15,6 +15,10 @@ import {TopPageImgTplComponent} from "../../_partials/top-page-img-tpl/top-page-
 import {CardModule} from "primeng/card";
 import {DropdownModule} from "primeng/dropdown";
 import {DbService} from "../../_services/db.service";
+import {InputMaskModule} from "primeng/inputmask";
+import {NG_VALIDATORS} from '@angular/forms';
+import {NgClass} from "@angular/common";
+import {Message, MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-register',
@@ -34,15 +38,17 @@ import {DbService} from "../../_services/db.service";
     PasswordModule,
     TopPageImgTplComponent,
     CardModule,
-    DropdownModule
+    DropdownModule,
+    InputMaskModule,
+    NgClass,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
   first_name?:string;
   last_name?:string;
-  email?:string;
+  email?:string = '';
   phone_number?:string;
   password?:string;
   confirm_password?:string;
@@ -75,6 +81,8 @@ export class RegisterComponent {
   selectedOnSiteGeneration?:any;
   onSiteGeneration?:any;
   otherOnSiteGeneration?:any;
+  //form!: FormGroup;
+  messages!: Message[];
 
   energyTypes:any = [
     {
@@ -176,58 +184,153 @@ export class RegisterComponent {
     }
   ]
 
-  constructor(private db: DbService) {
+
+  constructor(private db: DbService,private msg: MessageService) {
   }
 
   onSubmit = () => {
     const userObj = {
-      first_name:this.first_name,
-      last_name:this.last_name,
-      email:this.email,
-      phone_number:this.phone_number,
-      password:this.password,
-      confirm_password:this.confirm_password,
+      first_name: this.first_name,
+      last_name: this.last_name,
+      email: this.email,
+      phone_number: this.phone_number,
+      password: this.password,
+      confirm_password: this.confirm_password,
     }
 
-   const formObj = {
-     name: this.company_name,
-     address:this.address,
-     postcode:this.postcode,
-     uprn:this.uprn,
-     local_auth:this.local_auth,
-     est_year:this.est_year,
-     employees:this.employees,
-     turnover:this.turnover,
-     sector:this.sector,
-     sic_code:this.sic_code,
-     website_url:this.website_url,
-     company_description:this.company_description,
-     selectedEnergyType:this.selectedEnergyType,
-     energyTypeCost:this.energyTypeCost,
-     otherEnergyType:this.otherEnergyType,
-     selectedEnergyCost:this.selectedEnergyCost,
-     energyCostAdditional:this.energyCostAdditional,
-     selectedEnergyInfo:this.selectedEnergyInfo,
-     additionalEnergyInfo:this.additionalEnergyInfo,
-     selectedOnSiteGeneration:this.selectedOnSiteGeneration,
-     onSiteGeneration:this.onSiteGeneration,
-     otherOnSiteGeneration:this.otherOnSiteGeneration,
-     carbon_emissions:this.carbon_emissions,
-     development_issues:this.development_issues,
-     challenges:this.challenges,
-     decarb_plans:this.decarb_plans,
-     economy_opportunities:this.economy_opportunities,
-     policy:this.policy,
-     policy_idni:this.policy_idni
+    const formObj = {
+      name: this.company_name,
+      address: this.address,
+      postcode: this.postcode,
+      uprn: this.uprn,
+      local_auth: this.local_auth,
+      est_year: this.est_year,
+      employees: this.employees,
+      turnover: this.turnover,
+      sector: this.sector,
+      sic_code: this.sic_code,
+      website_url: this.website_url,
+      company_description: this.company_description,
+      selectedEnergyType: this.selectedEnergyType,
+      energyTypeCost: this.energyTypeCost,
+      otherEnergyType: this.otherEnergyType,
+      selectedEnergyCost: this.selectedEnergyCost,
+      energyCostAdditional: this.energyCostAdditional,
+      selectedEnergyInfo: this.selectedEnergyInfo,
+      additionalEnergyInfo: this.additionalEnergyInfo,
+      selectedOnSiteGeneration: this.selectedOnSiteGeneration,
+      onSiteGeneration: this.onSiteGeneration,
+      otherOnSiteGeneration: this.otherOnSiteGeneration,
+      carbon_emissions: this.carbon_emissions,
+      development_issues: this.development_issues,
+      challenges: this.challenges,
+      decarb_plans: this.decarb_plans,
+      economy_opportunities: this.economy_opportunities,
+      policy: this.policy,
+      policy_idni: this.policy_idni
 
-   }
+    }
 
-    this.db.addCompany(['*'],formObj,userObj).subscribe({
-      next:(res) => {
-        console.log(res)
-      }
-    })
 
+
+    if (this.checkInputs()){
+      this.db.addCompany(['*'], formObj, userObj).subscribe({
+        next: (res) => {
+          console.log(res)
+        }
+      })
+    } else{
+      this.msg.add({
+        severity:'warn',
+        detail:'Please fill in all of the mandatory fields.'
+      })
+    }
+
+  }
+
+  checkInputs = () => {
+    if (this.first_name?.length && this?.first_name?.length < 3) {
+      console.log(this.first_name?.length && this?.first_name?.length < 3);
+      this.messages = [{
+        severity: 'warn',
+        detail: 'Your first name must contain minimum 3 characters!'
+      }]
+      return false;
+    }
+    if (this.last_name?.length && this?.last_name?.length < 4) {
+      this.messages = [{
+        severity: 'warn',
+        detail: 'Your surname must contain minimum 4 characters!'
+      }]
+      return false;
+    }
+    if (this.phone_number?.length && this?.phone_number?.length < 8) {
+      this.messages = [{
+        severity: 'warn',
+        detail: 'Your mobile number must contain minimum 8 characters!'
+      }]
+      return false;
+    }
+    if (this.address?.length && this?.address.length < 5) {
+      this.messages = [{
+        severity: 'warn',
+        detail: 'Your address must contain minimum 5 characters!'
+      }]
+      return false;
+    }
+    if (!this.postcode?.length || this?.postcode.length < 5) {
+      this.messages = [{
+        severity: 'warn',
+        detail: 'Your postcode must contain minimum 6 characters!'
+      }]
+      return false;
+    }
+    if (!this.policy) {
+      return false;
+    }
+    const reg = /^([\w-\.]+@(?!lsu.edu)([\w-]+\.)+[\w-]{2,4})?$/;
+    if (!this.email?.length || reg.test(this.email)){
+      this.messages = [{
+        severity: 'warn',
+        detail: 'Invalid email!'
+      }]
+      return false;
+    }
+
+     return true;
+  }
+
+
+  // initForm = () => {
+  //   this.form = this._fb.group({
+  //     "first_name":['', [Validators.required]],
+  //     "last_name":['', [Validators.required]],
+  //     "email": ['', [Validators.required, Validators.email]],
+  //     "phone_number":['', [Validators.required]],
+  //     "company_name":['', [Validators.required]],
+  //     "address":['', [Validators.required]],
+  //     "postcode":['', [Validators.required]],
+  //     "policy":['', [Validators.required]],
+  //     "policy_idni": ['', [Validators.required]]
+  //   })
+  // }
+
+  onSave = () => {
+    // if(this.form.invalid){
+    //   this.messages.add=({
+    //     severity: 'warn',
+    //     detail: 'Please fill in all required fields'
+    //   })
+    //   return;
+    // }
+  }
+
+
+
+
+  ngOnInit(): void {
+    // this.initForm();
+    // this.onSave();
   }
 
 }
