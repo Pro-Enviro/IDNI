eccb80b2ce42a86f12ecae502fca3e399cef956e
mila <mila.lozanova@proenviro.co.uk>
2024-03-20 13:55:44 +0000

    top image added and page was structured

--- src/app/pages/knowledge-base/knowledge-base.component.ts
+++ src/app/pages/knowledge-base/knowledge-base.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {TopPageImgTplComponent} from "../../_partials/top-page-img-tpl/top-page-img-tpl.component";
+import {ToolbarModule} from "primeng/toolbar";
+import {InputTextModule} from "primeng/inputtext";
 
 @Component({
   selector: 'app-knowledge-base',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    RippleModule,
+    TopPageImgTplComponent,
+    ToolbarModule,
+    InputTextModule
+  ],
   templateUrl: './knowledge-base.component.html',
   styleUrl: './knowledge-base.component.scss'
 })
