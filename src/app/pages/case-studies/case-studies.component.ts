ca44877ac607732baea236496d74287ab1a43e04
mila <mila.lozanova@proenviro.co.uk>
2024-03-22 14:14:21 +0000

    case studies items api call made

--- src/app/pages/case-studies/case-studies.component.ts
+++ src/app/pages/case-studies/case-studies.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {TopPageImgTplComponent} from "../../_partials/top-page-img-tpl/top-page-img-tpl.component";
 import {DbService} from "../../_services/db.service";
+import {ButtonModule} from "primeng/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-case-studies',
   standalone: true,
   imports: [
-    TopPageImgTplComponent
+    TopPageImgTplComponent,
+    ButtonModule,
+    RouterLink
   ],
   templateUrl: './case-studies.component.html',
   styleUrl: './case-studies.component.scss'
@@ -18,10 +22,14 @@ export class CaseStudiesComponent {
     this.db.getContentFromCollection('case_studies/1', `
 ?fields=title,
 content,
-top_image
+top_image,
+studies.case_studies_items_id.title,
+studies.case_studies_items_id.content,
+studies.case_studies_items_id.image
 `).subscribe({
       next: (res: any) => {
         this.content = res
+        this.content.studies = this.content.studies.map((item:any) => item.case_studies_items_id)
       },
       error: (err: any) => {
         console.error(err)
