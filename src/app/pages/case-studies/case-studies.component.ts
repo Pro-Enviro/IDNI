d3707c21ec47d82c6e47755a0ee5128c57adadb5
mila <mila.lozanova@proenviro.co.uk>
2024-03-22 13:55:26 +0000

    api call made on case study page

--- src/app/pages/case-studies/case-studies.component.ts
+++ src/app/pages/case-studies/case-studies.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {TopPageImgTplComponent} from "../../_partials/top-page-img-tpl/top-page-img-tpl.component";
+import {DbService} from "../../_services/db.service";
 
 @Component({
   selector: 'app-case-studies',
   standalone: true,
-  imports: [],
+  imports: [
+    TopPageImgTplComponent
+  ],
   templateUrl: './case-studies.component.html',
   styleUrl: './case-studies.component.scss'
 })
 export class CaseStudiesComponent {
+  content:any;
 
+  constructor(private db: DbService) {
+    this.db.getContentFromCollection('case_studies/1', `
+?fields=title,
+content,
+top_image
+`).subscribe({
+      next: (res: any) => {
+        this.content = res
+      },
+      error: (err: any) => {
+        console.error(err)
+      }
+    })
+  }
 }
