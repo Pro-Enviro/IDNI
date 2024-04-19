b80c3cb6e0d28393ba468d9ade113da86ce94439
adamshelley31 <148070121+adamshelley31@users.noreply.github.com>
2024-04-19 16:01:48 +0100

    Added warning for users if they fail to login with correct credentials.

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -6,6 +6,8 @@ import { Router, RouterLink} from "@angular/router";
 import {AuthService, Credentials} from "../../_services/users/auth.service";
 import {FormsModule} from "@angular/forms";
 import {GlobalService} from "../../_services/global.service";
+import {from, of} from "rxjs";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-login',
@@ -28,13 +30,22 @@ export class LoginComponent {
   };
 
   constructor(
-    private  auth: AuthService
+    private  auth: AuthService,
+    private msg: MessageService
   ) {
   }
 
   login = () => {
     //if(this.credentials)
-    this.auth.login({...this.credentials, ...{mode: 'cookie'}})
+    from(this.auth.login({...this.credentials, ...{mode: 'cookie'}})).subscribe({
+      error: (err) => {
+        return this.msg.add({
+          severity: 'warn',
+          detail: 'Invalid credentials'
+        })
+      }
+    })
+
   }
 
 }
