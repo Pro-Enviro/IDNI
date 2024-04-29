44e828bbba13c7625f054b694e985fa3a1e65a3e
adamshelley31 <148070121+adamshelley31@users.noreply.github.com>
2024-04-29 10:24:32 +0100

    adding file-upload

--- src/app/pages/help/bug-report/bug-report.component.ts
+++ src/app/pages/help/bug-report/bug-report.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {SharedComponents} from "../../envirotrack/shared-components";
+import {uploadFiles} from "@directus/sdk";
 
 @Component({
   selector: 'app-bug-report',
@@ -9,7 +10,8 @@ import {SharedComponents} from "../../envirotrack/shared-components";
   imports: [
     ReactiveFormsModule,
     NgIf,
-    SharedComponents
+    SharedComponents,
+    NgForOf
   ],
   templateUrl: './bug-report.component.html',
   styleUrl: './bug-report.component.scss'
@@ -17,11 +19,20 @@ import {SharedComponents} from "../../envirotrack/shared-components";
 export class BugReportComponent implements OnInit {
 
   myForm!: FormGroup;
-  uploadedFiles: any;
+  uploadedFiles: any = [];
 
   constructor(private fb: FormBuilder) {}
 
+
+
+
+  uploadHandler = (event: any) => {
+      // this.uploadFiles.forEach((file: any) => this.myForm.append('screenshots', file))
+  }
+
   onSubmit(form: FormGroup) {
+
+    console.log(this.uploadedFiles)
     console.log('valid?', form.valid)
     console.log(form.value);
 
