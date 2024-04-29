7c3bba5191c4f9d852a6ebce0303a02a0b900b2e
adamshelley31 <148070121+adamshelley31@users.noreply.github.com>
2024-04-29 09:29:35 +0100

    Added contact form html and set up formcontrol group

--- src/app/pages/help/bug-report/bug-report.component.ts
+++ src/app/pages/help/bug-report/bug-report.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-bug-report',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './bug-report.component.html',
   styleUrl: './bug-report.component.scss'
 })
-export class BugReportComponent {
+export class BugReportComponent implements OnInit {
+
+  myForm: FormGroup = new FormGroup({});
+
+  email: FormControl<string | null > = new FormControl('')
+  issue: FormControl<string | null > = new FormControl('')
+  stepsToReproduce: FormControl<string | null > = new FormControl('')
+  screenshots: FormControl<File[] | null > = new FormControl([])
+  deviceUsed: FormControl<string | null > = new FormControl('')
+  browserUsed: FormControl<string | null > = new FormControl('')
+
+  constructor() {}
+
+  onSubmit(form: FormGroup) {
+    console.log(form.value)
+  }
+
+  ngOnInit(){
+    this.myForm = new FormGroup({
+      email: new FormControl(''),
+      issue: new FormControl(''),
+      stepsToReproduce: new FormControl(''),
+      screenshots: new FormControl([]),
+      deviceUsed: new FormControl(''),
+      browserUsed: new FormControl('')
+    });
+
+  }
 
 }
