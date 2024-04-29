15dfd336488cf49418cbd518d98f8468a494426a
mila <mila.lozanova@proenviro.co.uk>
2024-04-29 10:40:40 +0100

    dashboard faqs page - api call to fill the questions and answers, and old code was deleted

--- src/app/pages/help/faqs/faqs.component.ts
+++ src/app/pages/help/faqs/faqs.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {ButtonModule} from "primeng/button";
 import {AccordionModule} from "primeng/accordion";
+import {DbService} from "../../../_services/db.service";
 
 @Component({
   selector: 'app-faqs',
@@ -15,5 +16,17 @@ import {AccordionModule} from "primeng/accordion";
   styleUrl: './faqs.component.scss'
 })
 export class FaqsComponent {
-
+  content:any;
+    constructor(private db: DbService) {
+      this.db.getContentFromCollection('faqs_dashboard/1', `?fields=title,faqs_items.faqs_dashboard_ites_id.question,faqs_items.faqs_dashboard_ites_id.answer`).subscribe({
+        next: (res: any) => {
+          this.content = res
+          this.content.faqs_items = this.content.faqs_items.map((item:any) => item.faqs_dashboard_ites_id)
+        },
+        error: (err: any) => {
+          console.error(err)
+        }
+      })
+    }
 }
+
