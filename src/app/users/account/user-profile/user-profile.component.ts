a8e0079d7fb4960147069ffba40877b5a6628128
mila <mila.lozanova@proenviro.co.uk>
2024-05-30 15:39:38 +0100

    user panel- components added: p-card, inplace, button

--- src/app/users/account/user-profile/user-profile.component.ts
+++ src/app/users/account/user-profile/user-profile.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {CardModule} from "primeng/card";
+import {InplaceModule} from "primeng/inplace";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    InplaceModule,
+    InputTextModule,
+    FormsModule
+  ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
 export class UserProfileComponent {
-
+  first_name:any;
 }
