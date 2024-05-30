c5a88a965eaabc73ef3723c3f9a5f611ec69cabf
mila <mila.lozanova@proenviro.co.uk>
2024-05-30 16:27:34 +0100

    user panel- p-avatar component added on the user profile

--- src/app/users/account/user-profile/user-profile.component.ts
+++ src/app/users/account/user-profile/user-profile.component.ts
@@ -5,6 +5,7 @@ import {InputTextModule} from "primeng/inputtext";
 import {FormsModule} from "@angular/forms";
 import {InputGroupModule} from "primeng/inputgroup";
 import {InputGroupAddonModule} from "primeng/inputgroupaddon";
+import {AvatarModule} from "primeng/avatar";
 
 @Component({
   selector: 'app-user-profile',
@@ -15,7 +16,8 @@ import {InputGroupAddonModule} from "primeng/inputgroupaddon";
     InputTextModule,
     FormsModule,
     InputGroupModule,
-    InputGroupAddonModule
+    InputGroupAddonModule,
+    AvatarModule
   ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
