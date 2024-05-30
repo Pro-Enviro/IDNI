53d2a6e8392812957bd87450065fa44fda504735
adamshelley31 <148070121+adamshelley31@users.noreply.github.com>
2024-05-30 08:50:55 +0100

    Conditional container if submitted or not on forgotten password

--- src/app/users/forgot-password/forgot-password.component.ts
+++ src/app/users/forgot-password/forgot-password.component.ts
@@ -1,27 +1,27 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {AuthService} from "../../_services/users/auth.service";
-import {from, of} from "rxjs";
 import {MessageService} from "primeng/api";
 import {Router, RouterLink} from "@angular/router";
 import {ButtonModule} from "primeng/button";
 import {InputTextModule} from "primeng/inputtext";
 import {RippleModule} from "primeng/ripple";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [FormsModule, ButtonModule, InputTextModule, RippleModule, RouterLink],
+  imports: [FormsModule, ButtonModule, InputTextModule, RippleModule, RouterLink, NgIf],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
     email: string = ''
+  submitted: any = false;
 
   constructor(private auth: AuthService, private msg: MessageService, private router: Router) {}
 
   resetPassword =() => {
-      console.log('Resetting password')
 
     if (!this.email.length) return this.msg.add({
       severity: 'warn',
@@ -34,11 +34,13 @@ export class ForgotPasswordComponent {
           severity:'info',
           detail: 'Please check your email.'
         })
-        this.router.navigate(['/login'])
+
+        this.submitted = true;
       }
 
     });
 
 
   }
+
 }
