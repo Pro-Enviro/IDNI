ecf1a3644f440c9f32800b2ec98d6566b484635a
mila <mila.lozanova@proenviro.co.uk>
2024-07-01 16:30:27 +0100

    bar chart - selectedCompany is number and ngInit () updated

--- src/app/pages/envirotrack/report/envirotrack-report-bar/envirotrack-bar-small/envirotrack-bar-small.component.ts
+++ src/app/pages/envirotrack/report/envirotrack-report-bar/envirotrack-bar-small/envirotrack-bar-small.component.ts
@@ -34,7 +34,7 @@ export class EnvirotrackBarSmallComponent implements OnInit {
   months: string[] = [];
   filteredData: any;
   companies: any;
-  selectedCompany!: number | null;
+  selectedCompany!: number;
   chartData: any = [];
   chartX: string[] = [];
   chartY: string[] = [];
@@ -206,8 +206,9 @@ export class EnvirotrackBarSmallComponent implements OnInit {
 
   onSelectCompany = () => {
     // this.global.updateSelectedMpan(this.selectedMpan)
-    this.selectedCompany ? this.global.updateCompanyId(this.selectedCompany) : null
-    this.selectedCompany ? this.getData(this.selectedCompany) : null
+
+    this.track.updateSelectedCompany(this.selectedCompany)
+    this.getData(this.selectedCompany)
   }
 
   getTimes = () => {
@@ -285,9 +286,9 @@ export class EnvirotrackBarSmallComponent implements OnInit {
   ngOnInit() {
     this.getCompanies();
 
-    if (this.global.companyAssignedId.value) {
-      this.selectedCompany = this?.global?.companyAssignedId?.value || null;
-      this.getData(this?.global?.companyAssignedId?.value)
+    if (this.track.selectedCompany.value) {
+      this.selectedCompany = this.track.selectedCompany.value
+      this.getData(this.selectedCompany)
     }
 
     this.screenWidth = window.innerWidth
