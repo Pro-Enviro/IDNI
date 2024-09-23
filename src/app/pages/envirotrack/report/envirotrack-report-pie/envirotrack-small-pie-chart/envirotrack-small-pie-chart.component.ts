b52b469392e94eee241dc9f2d10237fd96a68125
mila <mila.lozanova@proenviro.co.uk>
2024-09-23 13:09:09 +0100

    if the selectedMpan is part of the mpan array all mpan should be selectable - code changed for small pie chart

--- src/app/pages/envirotrack/report/envirotrack-report-pie/envirotrack-small-pie-chart/envirotrack-small-pie-chart.component.ts
+++ src/app/pages/envirotrack/report/envirotrack-report-pie/envirotrack-small-pie-chart/envirotrack-small-pie-chart.component.ts
@@ -244,11 +244,11 @@ export class EnvirotrackSmallPieChartComponent implements OnInit {
             !~this.mpan.indexOf(row.mpan) ? this.mpan.push(row.mpan) : null;
           })
 
-          // if (this.global.selectedMpan?.value) {
-          //   this.selectedMpan = this.global.selectedMpan.value
-          // } else {
-          this.selectedMpan === undefined ? this.selectedMpan = this.mpan[0] : null
-          // }
+          if (this.selectedMpan && this.mpan.includes(this.selectedMpan)){
+            this.selectedMpan = this.global.selectedMpan.value
+          } else {
+            this.selectedMpan = this.mpan[0]
+          }
 
           this.data = res
           this.getTimes()
