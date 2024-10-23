09a7f2457f4ad358905550f0751981139e0c8a36
Adam Shelley <adam.shelley@proenviro.co.uk>
2024-10-23 12:24:15 +0800

    Rewrote functionality to handle duplicate recommendations/digital twin data.

--- src/app/pages/digital-twins/dt-report/dt-report.component.ts
+++ src/app/pages/digital-twins/dt-report/dt-report.component.ts
@@ -11,6 +11,7 @@ import {SharedComponents} from "../../envirotrack/shared-components";
 import {CarouselModule} from "primeng/carousel";
 import {calculateEnergyData} from "./calculateEnergyData";
 import Fuse from "fuse.js";
+import {mergeDuplicateSolutions} from "./mergeDuplicateSolutions";
 
 @Component({
   selector: 'app-dt-report',
@@ -66,12 +67,7 @@ export class DtReportComponent {
   }
 
 
-  sumProperties = (item1: any, item2: any) => {
-    item1.estimatedEnergySaving += item2?.estimatedEnergySaving || 0;
-    item1.estimatedCarbonSaving += item2?.estimatedCarbonSaving || 0;
-    item1.estimatedCost += item2?.estimatedCost || 0;
-    item1.estimatedSaving += item2?.estimatedSaving || 0;
-  }
+
 
   onClusterSelect = (event: any) => {
     this.availableRecommendations = []
@@ -125,61 +121,9 @@ export class DtReportComponent {
       return reco;
     })
 
-    // Go through recommendations are collate similar named ones
-    const fuseOptions = {
-      threshold: 0.2,
-      ignoreLocation: true,
-      includeScore: true,
-      keys: ['recommendation']
-    }
 
-    const fuse = new Fuse(this.availableRecommendations, fuseOptions);
-
-
-    this.availableRecommendations.forEach((reco: any) => {
-      const foundDuplicates = fuse.search(reco.recommendation)
-        .filter(result => result.item !== reco)
-        .map(result => result.item);
-
-      let paybackSum = reco.paybackPeriod || 0;
-      let paybackCount = reco.counter || 1;
-
-      if (foundDuplicates.length > 0) {
-        foundDuplicates.forEach(duplicate => {
-          this.sumProperties(reco, duplicate);
-
-          // Get payback period
-          if (duplicate.paybackPeriod) {
-            paybackSum += duplicate.paybackPeriod;
-            paybackCount++;
-          }
-
-          // Remove duplicate from recommendations
-          const indexToRemove = this.availableRecommendations.indexOf(duplicate);
-          if (indexToRemove !== -1) {
-            this.availableRecommendations.splice(indexToRemove, 1);
-          }
-
-          // Average of Payback periods
-
-          // Add counter to current Reco
-          if (reco.counter) reco.counter++;
-          else reco.counter = 1;
-        })
-        reco.paybackPeriod = paybackSum / paybackCount;
-      }
-    })
-
-    // BMI Trailers
-    // Axon Power & Control Ltd
-    // McCloskey International- Granville
-    // Linergy LTD
-    // ABP Linden
-    // Linden Foods
-    // Dunbia (UK)
-    // Westland Horticulture Ltd
-    // Dunbia - Hides
-    // Dunbia - Retail
+    const mergedRecommendations = mergeDuplicateSolutions(this.availableRecommendations)
+    this.availableRecommendations = mergedRecommendations
 
 
     // Select solution recommendations from report page
@@ -197,51 +141,9 @@ export class DtReportComponent {
     // Filter if not text is available
     this.availableDigitalTwinData = this.availableDigitalTwinData.filter((company: any) => company?.solutionText)
 
+    const mergedDigitalTwinData = mergeDuplicateSolutions(this.availableDigitalTwinData, 'digitalTwin')
+    this.availableDigitalTwinData = mergedDigitalTwinData
 
-    const fuseTwinsOptions = {
-      threshold: 0.2,
-      ignoreLocation: true,
-      includeScore: true,
-      keys: ['solutionText']
-    }
-
-    const fuseTwins = new Fuse(this.availableDigitalTwinData, fuseTwinsOptions);
-
-
-    this.availableDigitalTwinData.forEach((reco: any) => {
-
-      const foundDuplicates = fuseTwins.search(reco.solutionText)
-        .filter(result => result.item !== reco)
-        .map(result => result.item);
-
-      let paybackSum = reco.paybackPeriod || 0;
-      let paybackCount = reco.counter || 1;
-
-      if (foundDuplicates.length > 0) {
-
-        foundDuplicates.forEach(duplicate => {
-          this.sumProperties(reco, duplicate);
-
-          // Get payback period
-          if (duplicate.paybackPeriod) {
-            paybackSum += duplicate.paybackPeriod;
-            paybackCount++;
-          }
-
-          // Remove duplicate from recommendations
-          const indexToRemove = this.availableDigitalTwinData.indexOf(duplicate);
-          if (indexToRemove !== -1) {
-            this.availableDigitalTwinData.splice(indexToRemove, 1);
-          }
-
-          // Add counter to current Reco
-          if (reco.counter) reco.counter++;
-          else reco.counter = 1;
-        })
-
-        reco.paybackPeriod = paybackSum / paybackCount;
-      }
-    })
 
 
     // Get Non-HH energy data
---
+++ src/app/pages/digital-twins/dt-report/mergeDuplicateSolutions.ts
@@ -0,0 +1,101 @@
+import Fuse from "fuse.js";
+
+const FUSE_OPTIONS = {
+  recommendation: {
+    threshold: 0.2,
+    ignoreLocation: true,
+    includeScore: true,
+    keys: ['recommendation']
+  },
+  digitalTwin: {
+    threshold: 0.4,
+    ignoreLocation: true,
+    includeScore: true,
+    keys: ['solutionText']
+  }
+}
+
+const PROPERTIES_TO_MERGE = ['estimatedEnergySaving',
+  'estimatedCarbonSaving',
+  'estimatedCost',
+  'estimatedSaving',]
+
+// Implement fuzzy matching for recommendations and Digital Twin solutions
+export function mergeDuplicateSolutions(items: any, type = 'recommendation') {
+  if (!items?.length) return []
+
+  const workingSet = [...items];
+  const fuseOptions = type === 'digitalTwin' ? FUSE_OPTIONS.digitalTwin : FUSE_OPTIONS.recommendation
+  const fuse = new Fuse(workingSet, fuseOptions);
+  const processedIds = new Set();
+  const mergedItems: any[] = [];
+
+  workingSet.forEach((item, index: number) => {
+    if (processedIds.has(index)) return;
+    const searchKey = type === 'digitalTwin' ? item.solutionText : item.recommendation
+
+    if (!searchKey) return;
+
+    const searchResults = fuse.search(searchKey);
+
+    const duplicates = searchResults.filter((result: any) => {
+      const resultIndex = workingSet.indexOf(result.item);
+      return resultIndex !== index && !processedIds.has(resultIndex)
+    }).map(result => ({
+      item: result.item,
+      index: workingSet.indexOf(result.item)
+    }))
+
+
+    if (duplicates.length > 0) {
+      const mergedItem = mergeDuplicateItem(item, duplicates.map(d => d.item));
+      duplicates.forEach(duplicate => processedIds.add(duplicate.index));
+      mergedItems.push(mergedItem);
+    } else if (!processedIds.has(index)) {
+      mergedItems.push({...item})
+    }
+
+    processedIds.add(index);
+  })
+
+  return mergedItems;
+}
+
+
+function mergeDuplicateItem(original: any, duplicates: any) {
+  const merged = {...original}
+  let paybackSum = merged.paybackPeriod || 0;
+  let paybackCount = merged.paybackPeriod ? 1 : 0;
+  merged.counter = 1;
+
+  duplicates.forEach((duplicate: any) => {
+    merged.counter = (merged.counter || 1) + 1;
+
+    // Handle payback
+    if (duplicate?.paybackPeriod != null) {
+      paybackSum += duplicate.paybackPeriod;
+      paybackCount++;
+    }
+
+    // Sum all required properties
+    PROPERTIES_TO_MERGE.forEach((prop: any) => {
+      if (duplicate[prop] != null) {
+        merged[prop] = (merged[prop] || 0) + duplicate[prop];
+      }
+    })
+  })
+  // Calculate average payback
+  if (paybackCount > 0) {
+    merged.paybackPeriod = paybackSum / paybackCount;
+  }
+
+
+  return merged;
+}
+
+// sumProperties = (item1: any, item2: any) => {
+//   item1.estimatedEnergySaving += item2?.estimatedEnergySaving || 0;
+//   item1.estimatedCarbonSaving += item2?.estimatedCarbonSaving || 0;
+//   item1.estimatedCost += item2?.estimatedCost || 0;
+//   item1.estimatedSaving += item2?.estimatedSaving || 0;
+// }
