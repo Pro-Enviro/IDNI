9bdc40f7b89da49cdf639ee4a2c792af763ca2fb
Adam Shelley <adam.shelley@proenviro.co.uk>
2024-10-24 16:46:18 +0800

    Working on improving interceptor. To prevent requiring refresh.

--- src/app/_helpers/http-interceptor.service.ts
+++ src/app/_helpers/http-interceptor.service.ts
@@ -5,12 +5,14 @@ import {
   HttpInterceptorFn,
   HttpRequest,
 } from "@angular/common/http";
-import {catchError, from, switchMap, throwError} from "rxjs";
+import {catchError, from, Observable, switchMap, throwError} from "rxjs";
 import {StorageService} from "../_services/storage.service";
 import {inject} from '@angular/core';
 import {Router} from "@angular/router";
 import {MessageService} from "primeng/api";
 import {AuthService} from "../_services/users/auth.service";
+
+let refreshTokenPromise: Promise<string> | null = null;
 
 
 export const HttpInterceptorService: HttpInterceptorFn = (
@@ -21,73 +23,74 @@ export const HttpInterceptorService: HttpInterceptorFn = (
   const storage = inject(StorageService)
   const router = inject(Router)
   const msg = inject(MessageService)
-  const token = storage.get('access_token');
 
+
+
+  // If refresh is happening just continue on
+  if (req.url.includes('refresh')) {
+    return next(req);
+  }
+
+  // Helper function to add the bearer token to the request
   const addTokenHeader = (request: HttpRequest<any>, token: string | null) => {
     return request.clone({
       headers: request.headers.set('Authorization', 'Bearer ' + token!),
     });
   }
 
-  if (token){
-    const authRequest = addTokenHeader(req, token)
+  const handleLogout = () => {
+    storage.clearAll();
+    refreshTokenPromise = null;
+    router.navigate(['login']);
+    msg.add({
+      severity: 'info',
+      summary: 'Session Expired',
+      detail: 'Please login again to continue.'
+    });
+  };
 
+  const handleRequest = (token: string | null): Observable<any> => {
+    const authRequest = token ? addTokenHeader(req, token) : req;
+
+    // If the token is not refreshing, start the refresh
     return next(authRequest).pipe(
       catchError((error: HttpErrorResponse) => {
-        if ([401, 403].includes(error.status)) {
-          if (error.status === 401) {
-            console.log('401 - getting refresh token')
-            return from(auth.refreshToken()).pipe(
-              switchMap((token: any) => {
-                const newRequest = addTokenHeader(req, token)
-                console.log('New token request', newRequest)
-                window.location.reload()
-                console.log('NEXT REQUEST')
-                return next(newRequest)
-              })
-            )
-          } else if (error.status === 403) {
-            localStorage.clear()
-            router.navigate(['login'])
-          } else {
-            router.navigate(['login'])
+        if (error.status === 401) {
+          if (!refreshTokenPromise) {
+            refreshTokenPromise = auth.refreshToken().then((newToken) => {
+              refreshTokenPromise = null;
+              return newToken;
+            }).catch((err) => {
+              refreshTokenPromise = null;
+              handleLogout();
+              throw err;
+            })
           }
-        }
-        msg.add({
-          severity: 'warning',
-          detail: 'You are unauthorized.'
-        })
-        return throwError(() => error);
-      }))
-  } else {
-    // If the user has no token (not logged in)
-    return next(req).pipe(
-      catchError((error: HttpErrorResponse) => {
-        // console.log('ERROR 2: ', error)
-        if ([401, 403].includes(error.status)) {
-          // auto logout if 401 or 403 response returned from api
-          if (error.status === 401) {
-            console.log('401 Error')
-            // handle 401
 
-          } else if (error.status === 403) {
-            console.log('403 Error')
-            // handle 403
-            localStorage.clear()
-            router.navigate([''])
-          } else {
-            localStorage.clear()
-            router.navigate(['login'])
-          }
-          router.navigate(['login'])
+          // Once refresh is received, retry the original request
+          return from(refreshTokenPromise).pipe(
+            switchMap(newToken => {
+              const newRequest = addTokenHeader(req, newToken);
+              return next(newRequest);
+            }),
+            catchError(refreshError => {
+              console.error('Token refresh failed: ' , refreshError);
+              handleLogout();
+              return throwError(() => refreshError)
+            })
+          )
         }
-        msg.add({
-          severity: 'warning',
-          detail: 'You are unauthorized.'
-        })
-
+        if (error.status === 403) {
+          handleLogout();
+          return throwError(() => error);
+        }
 
         return throwError(() => error);
-      }))
+      })
+    )
   }
+
+  const token = storage.get('access_token');
+  return handleRequest(token)
+
 };
